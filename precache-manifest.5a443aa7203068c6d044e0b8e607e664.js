ae8050ba21e4fb4c81089302156d77233723506b
andy <andy.william.mcdonald@gmail.com>
2020-08-27 11:32:52 +0100

    Updates

---
+++ precache-manifest.5a443aa7203068c6d044e0b8e607e664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9089e00702755d6b7148bbb49227d80",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "a9ebdaece0b95ff9cea6",
+    "url": "/pomodoro-clock/static/css/main.c005e64f.chunk.css"
+  },
+  {
+    "revision": "2a1c8d12bdf4990a6b64",
+    "url": "/pomodoro-clock/static/js/2.488f0cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.488f0cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9ebdaece0b95ff9cea6",
+    "url": "/pomodoro-clock/static/js/main.3cf89f18.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
